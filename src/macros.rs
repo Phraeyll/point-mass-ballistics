fe973fe4f5848c241d4b45bd767d68b5ea0d5229
Phraeyll <Phraeyll@users.noreply.github.com>
2018-09-16 13:11:09 -0600

    Use FloatMap instead of DragTable, more generic.

--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,13 @@
+use of::OrderedFloat;
+
+use std::collections::BTreeMap;
+
+pub struct FloatMap(pub BTreeMap<OrderedFloat<f64>, f64>);
+
 macro_rules! drag_table {
     ( $($mach:expr => $cd:expr,)+ ) => ( drag_table!($($mach => $cd),+) );
     ( $($mach:expr => $cd:expr),* ) => {{
-        let mut _drag_table = DragTable(BTreeMap::new());
+        let mut _drag_table = FloatMap(BTreeMap::new());
         $(
             let _ = _drag_table.0.insert(OrderedFloat($mach), $cd);
         )*
