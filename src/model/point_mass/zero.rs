a675733a650abf59c5fefa934134fe37d81cd7a0
Phraeyll <Phraeyll@users.noreply.github.com>
2018-12-30 23:16:31 -0700

    Change zero to use an iterator internally - perhaps add more genericity, but no longer returning reason iteration stops (not sure if there is a workaround for this).

--- src/model/point_mass/zero.rs
+++ src/model/point_mass/zero.rs
@@ -2,51 +2,72 @@ use approx::relative_eq;
 
 use crate::util::{Numeric, FRAC_PI_4};
 
-impl super::Simulation<'_> {
-    // Find muzzle angle to achieve 0 drop at specified distance, relative to scope height
-    pub(crate) fn zero(&mut self) -> Result<Numeric, String> {
+// This angle will trace the longest possible trajectory for a projectile (45 degrees)
+const MAX_ANGLE: Numeric = FRAC_PI_4;
+
+struct IterZero<'s> {
+    iter: &'s mut super::Simulation<'s>,
+    angle: Numeric,
+    drop: Numeric,
+}
+impl<'s> Iterator for IterZero<'s> {
+    type Item = (Numeric, Numeric);
+    fn next(&mut self) -> Option<Self::Item> {
+        let muzzle_pitch = self.iter.muzzle_pitch;
+        let drop = self.drop;
+        let zero_distance = Numeric::from(self.iter.zero_distance.to_meters());
+
+        // Increment/decrement pitch before iteration below
+        self.iter.muzzle_pitch += self.angle;
+
+        // Maximum angle or muzzle_pitch not changing due to very small angle (floating point limitation)
+        if self.iter.muzzle_pitch > MAX_ANGLE || self.iter.muzzle_pitch == muzzle_pitch {
+            return None;
+        }
+        // Find height in meters relative to zero, given pitch
+        if let Some(projectile) = self
+            .iter
+            .iter()
+            .find(|p| p.relative_position().x >= zero_distance)
+        {
+            self.drop = projectile.relative_position().y;
+        } else {
+            // Terminal velocity reached
+            return None;
+        };
+        // Change direction and angle by 1/2 if
+        // above(positive drop) and going   up(positive angle) or
+        // below(negative drop) and going down(negative angle)
+        if self.angle.is_sign_positive() ^ self.drop.is_sign_negative() {
+            self.angle *= -1.0;
+        }
+        self.angle /= 2.0;
+
+        Some((muzzle_pitch, drop))
+    }
+}
+
+impl<'s> super::Simulation<'s> {
+    fn zero_iter(&'s mut self) -> IterZero {
         // This angle will trace the longest possible trajectory for a projectile (45 degrees)
-        const MAX_ANGLE: Numeric = FRAC_PI_4;
         // Start with maximum angle to allow for zeroing at longer distances
-        let mut angle = MAX_ANGLE;
-        loop {
-            let muzzle_pitch: Numeric = self.muzzle_pitch;
-            self.muzzle_pitch += angle;
-            if self.muzzle_pitch > MAX_ANGLE {
-                break Err(format!(
-                    "Can never 'zero' at this range: {}",
-                    Numeric::from(self.zero_distance)
-                ));
-            }
-            if self.muzzle_pitch == muzzle_pitch {
-                break Err(format!(
-                    "Issue with floating points, pitch: {} by angle: {} not changing during 'zero'",
-                    muzzle_pitch, angle,
-                ));
-            }
-            let drop = if let Some(projectile) = self
-                .iter()
-                .find(|p| p.relative_position().x >= Numeric::from(self.zero_distance.to_meters()))
-            {
-                projectile.relative_position().y
-            } else {
-                break Err(format!(
-                    "Reach terminal velocity 'zeroing' for this range: {}",
-                    Numeric::from(self.zero_distance)
-                ));
-            };
-            // Quit once zero point is found, once drop is equal to zero
-            if relative_eq!(drop, 0.0) {
-                break Ok(self.muzzle_pitch);
-            }
-            // If in the following states (xor), change direction by flipping angle sign
-            // true, false || false, true
-            // up,   above || down,  below
-            if angle.is_sign_positive() ^ drop.is_sign_negative() {
-                angle *= -1.0;
-            }
-            // Reduce angle before next iteration, trying to converge on zero point
-            angle /= 2.0;
+        IterZero {
+            iter: self,
+            angle: MAX_ANGLE,
+            drop: -1.0,
+        }
+    }
+}
+impl<'s> super::Simulation<'s> {
+    // Find muzzle angle to achieve 0 drop at specified distance, relative to scope height
+    pub(crate) fn zero(&'s mut self) -> Result<Numeric, String> {
+        if let Some((muzzle_pitch, _)) =
+            self.zero_iter()
+                .find(|(_, drop)| relative_eq!(*drop, 0.0))
+        {
+            Ok(muzzle_pitch)
+        } else {
+            Err(String::from("Cannot zero for this range"))
         }
     }
 }
