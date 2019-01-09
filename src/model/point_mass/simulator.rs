ab2457ba4e1258edc1abe054ec87b820f73a0198
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-08 20:00:42 -0700

    Renamed Simulator to SimulationBuilder - keep zero_distance here, simplifies drop_table function.

--- src/model/point_mass/simulator.rs
+++ src/model/point_mass/simulator.rs
@@ -5,19 +5,21 @@ use super::{iter::Output, *};
 // Distance => (drop, windage, velocity, energy, moa, time)
 type TableVal = (Numeric, Numeric, Numeric, Numeric, Numeric, Numeric);
 
-pub struct Simulator<'p> {
+pub struct SimulationBuilder<'p> {
     pub projectile: &'p Projectile, // Model variables, mostly projectile properties
     pub scope: &'p Scope,           // Model variables, mostly projectile properties
     pub zero_conditions: &'p Conditions,
     pub solve_conditions: &'p Conditions,
+    pub zero_distance: Length,
     pub time_step: Numeric,
 }
-impl<'p> Simulator<'p> {
+impl<'p> SimulationBuilder<'p> {
     pub fn new(
         projectile: &'p Projectile,
         scope: &'p Scope,
         zero_conditions: &'p Conditions,
         solve_conditions: &'p Conditions,
+        zero_distance: Numeric,
         time_step: Numeric,
     ) -> Self {
         Self {
@@ -25,47 +27,42 @@ impl<'p> Simulator<'p> {
             scope,
             zero_conditions,
             solve_conditions,
+            zero_distance: Length::Yards(zero_distance),
             time_step,
         }
     }
     // Create simulation with conditions used to find muzzle_pitch for 'zeroing'
     // Starting from flat fire pitch (0.0)
-    fn zero_simulation(&self, zero_distance: Length) -> Simulation {
+    fn zero_simulation(&self) -> Simulation {
         Simulation::new(
             &self.projectile,
             &self.scope,
             &self.zero_conditions,
             0.0,
-            zero_distance,
+            self.zero_distance,
             self.time_step,
         )
     }
     // Create a simulation with muzzle pitch found in 'zeroin' simulation
     // Then solve for current conditions
     // Can be used for drop table, or eventually dialing in a specific distance
-    fn solution_simulation(&self, zero_distance: Length, offset: Numeric) -> Simulation {
+    fn solution_simulation(&self, offset: Numeric) -> Simulation {
         Simulation::new(
             &self.projectile,
             &self.scope,
             &self.solve_conditions,
-            match self.zero_simulation(zero_distance).zero() {
+            match self.zero_simulation().zero() {
                 Ok(muzzle_pitch) => muzzle_pitch + (offset / 60.0).to_radians(),
                 Err(err) => panic!(err),
             },
-            zero_distance,
+            self.zero_distance,
             self.time_step,
         )
     }
     // Produce a drop table using specified range and step size
-    pub fn drop_table(
-        &self,
-        zero_distance: Numeric,
-        step: u32,
-        range: u32,
-        offset: Numeric,
-    ) -> FloatMap<TableVal> {
+    pub fn drop_table(&self, step: u32, range: u32, offset: Numeric) -> FloatMap<TableVal> {
         let mut current_step: u32 = 0; // This can overflow, not sure how to check
-        self.solution_simulation(Length::Yards(zero_distance), offset)
+        self.solution_simulation(offset)
             .iter()
             .do_take_while(|p| p.distance() <= Numeric::from(range))
             .filter_map(|p| {
