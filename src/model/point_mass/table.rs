4850d728803811389359ba19e3612c78336f1111
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-10 13:51:10 -0700

    Fusing does help here - stops after first terminal velocity reached, if one is reached.

--- src/model/point_mass/table.rs
+++ src/model/point_mass/table.rs
@@ -6,7 +6,7 @@ use super::{
 impl super::Simulation<'_> {
     // Produce a drop table using specified range and step size
     pub fn drop_table(&self, step: u32, range: u32) -> FloatMap<Packet<'_>> {
-        let mut iter = self.iter();
+        let mut iter = self.iter().fuse();
         (0..=range)
             .step_by(step as usize)
             .filter_map(|current_step| {
