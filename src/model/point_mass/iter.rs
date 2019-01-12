154ed8e99b80c2528ddc5ab70d8c261b4dc28042
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-11 21:10:08 -0700

    Need to factor in all 'vertical' angles for coriolis - was showing wrong windage values when chaing either line of sight, or adjust pitch offset.

--- src/model/point_mass/iter.rs
+++ src/model/point_mass/iter.rs
@@ -91,12 +91,12 @@ impl IterSimulation<'_> {
     // Bearing East results in higher elevation (+y absolute/relative)
     // Bearing West results in lower elevation (-y relative/absolute)
     fn coriolis_acceleration(&self) -> Vector3<Numeric> {
-        -2.0 * self
-            .simulation
-            .conditions
-            .other
-            .omega()
-            .cross(&self.velocity)
+        let omega = self.simulation.conditions.other.omega().pivot_z(
+            self.simulation.conditions.other.lattitude()
+                + self.simulation.conditions.other.line_of_sight()
+                + self.simulation.muzzle_pitch,
+        );
+        -2.0 * omega.cross(&self.velocity)
     }
     // Velocity relative to speed of sound (c), with given atmospheric conditions
     fn mach(&self) -> Numeric {
