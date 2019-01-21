5b0bc02b91b2b7a174e7cf3eea7ed9d0ee45bafd
Phraeyll <Phraeyll@users.noreply.github.com>
2019-01-20 23:50:12 -0700

    Rethink macro - just use aliases for now, macro was more writing with latest change anyways

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,19 @@
 #[macro_use]
 mod util {
-    // Determine which type to use dynamically, accounts for f32/f64 consts as well.
-    // Can't use ident - need to check token to determine Natural type
-    macro_rules! numeric {
-        ( f32 ) => {
-            use std::f32::consts;
-            pub type Numeric = f32;
-            pub type Natural = u16;
-        };
-        ( f64 ) => {
-            use std::f64::consts;
-            pub type Numeric = f64;
-            pub type Natural = u32;
-        };
-    }
-    numeric!(f64);
-    pub const PI: Numeric = consts::PI;
-    pub const FRAC_PI_4: Numeric = consts::FRAC_PI_4;
-    pub const FRAC_PI_2: Numeric = consts::FRAC_PI_2;
-
     pub use conversions::*;
     pub use float_map::*;
     pub use nalgebra_helpers::*;
 
-    #[allow(clippy::or_fun_call)]
+    pub type Numeric = f64;
+    pub type Natural = u32;
+
+    use std::f64::consts;
+    pub const PI: Numeric = consts::PI;
+    pub const FRAC_PI_4: Numeric = consts::FRAC_PI_4;
+    pub const FRAC_PI_2: Numeric = consts::FRAC_PI_2;
+
     #[macro_use]
+    #[allow(clippy::or_fun_call)]
     mod float_map;
     mod conversions {
         // Terribly inefficient and unsafe/untyped method of unit conversion, only for units needed
