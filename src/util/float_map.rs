00e301aa8036decd8ed6a781bab9b43aedb6e3e6
Phraeyll <Phraeyll@users.noreply.github.com>
2019-07-16 19:04:38 -0600

    Move all OrderedFloat business into trait impls - no need to manually unwrap during linar interpolation.

--- src/util/float_map.rs
+++ src/util/float_map.rs
@@ -3,7 +3,7 @@ use crate::{
     util::Numeric,
 };
 
-use std::{collections::BTreeMap, iter::FromIterator, ops::RangeBounds};
+use std::{collections::BTreeMap, iter::FromIterator, ops::{RangeBounds, Bound}};
 
 use ordered_float::OrderedFloat;
 
@@ -48,17 +48,28 @@ impl<V> FloatMap<V> {
     pub fn insert(&mut self, key: Numeric, value: V) -> Option<V> {
         self.0.insert(OrderedFloat(key), value)
     }
-    pub fn iter(&self) -> impl Iterator<Item = (&OrderedFloat<Numeric>, &V)> {
-        self.0.iter()
+    pub fn iter(&self) -> impl Iterator<Item = (&Numeric, &V)> {
+        self.0.iter().map(|(OrderedFloat(key), val)| (key, val))
     }
     pub fn range<R>(
         &self,
         range: R,
-    ) -> impl DoubleEndedIterator<Item = (&OrderedFloat<Numeric>, &V)>
+    ) -> impl DoubleEndedIterator<Item = (&Numeric, &V)>
     where
-        R: RangeBounds<OrderedFloat<Numeric>>,
+        R: RangeBounds<Numeric>,
     {
-        self.0.range(range)
+        use Bound::*;
+        fn wrap_bound(bound: Bound<&Numeric>) -> Bound<OrderedFloat<Numeric>> {
+            match bound {
+                Unbounded => Unbounded,
+                Excluded(f) => Excluded(OrderedFloat(*f)),
+                Included(f) => Included(OrderedFloat(*f)),
+            }
+        }
+        let start = wrap_bound(range.start_bound());
+        let end = wrap_bound(range.end_bound());
+
+        self.0.range((start, end)).map(|(OrderedFloat(key), val)| (key, val))
     }
 }
 
@@ -68,12 +79,11 @@ impl FloatMap<Numeric> {
     // and use them along with their values for interpolation
     // Works for exact values of 'x' as well
     pub fn lerp(&self, x: Numeric) -> Result<Numeric> {
-        let key = OrderedFloat(x);
-        self.range(..key)
+        self.range(..x)
             .rev()
-            .zip(self.range(key..))
+            .zip(self.range(x..))
             .next()
-            .map(|((OrderedFloat(x0), y0), (OrderedFloat(x1), y1))| {
+            .map(|((x0, y0), (x1, y1))| {
                 y0 + (x - x0) * ((y1 - y0) / (x1 - x0))
             })
             .ok_or(Error::new(ErrorKind::VelocityLookup(x)))
