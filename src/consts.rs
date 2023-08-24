4a69f0c3178c7d2994492c88ff01d2a7b1544785
Phraeyll <Phraeyll@users.noreply.github.com>
2023-08-24 16:57:40 -0600

    Simplify const exports

--- src/consts.rs
+++ src/consts.rs
@@ -1,8 +1,6 @@
-use crate::Numeric;
-
-use std::f64::consts;
-
-pub const PI: Numeric = consts::PI;
-pub const FRAC_PI_2: Numeric = consts::FRAC_PI_2;
-pub const FRAC_PI_4: Numeric = consts::FRAC_PI_4;
-pub const FRAC_PI_8: Numeric = consts::FRAC_PI_8;
+pub use std::f64::consts::{
+    PI,
+    FRAC_PI_2,
+    FRAC_PI_4,
+    FRAC_PI_8,
+};
